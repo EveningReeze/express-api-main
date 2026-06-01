940f6c54317d5dfe60d35819e5fe08b9c56eb452
EveningReeze <z17690728020@163.com>
2026-06-01 15:01:08 +0300

    tijiao Docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,13 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base', // 使用 Airbnb 风格
+    'airbnb-base', 
   ],
   parserOptions: {
-    ecmaVersion: 2018, // 注意这里是 2018
+    ecmaVersion: 2018, 
   },
   rules: {
-    // 这里是自定义规则区域
-    // 例如：强制使用分号，禁止使用 console
+   
     'semi': ['error', 'always'],
     'no-console': 'off',
   }
